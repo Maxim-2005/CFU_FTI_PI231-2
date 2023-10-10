6a25d39748afd2fdba6048f60d58ca70e1731e91
Maxim <55892094+Maxim-2005@users.noreply.github.com>
2023-10-10 22:31:26 +0300

    Update source.cpp

--- 1 Курс/Contest_01/5.Индекс массы тела/source.cpp
+++ 1 Курс/Contest_01/5.Индекс массы тела/source.cpp
@@ -1,13 +1,17 @@
+// Подключение библиотек
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main(){
+    // Объявление переменных
     double m = 0;
     double h = 0;
-    
+
+    // Получение значений с консоли
     cin >> m, cin >> h;
-    
+
+    // Расчет + вывод
     cout << m / (h*h);
 }
