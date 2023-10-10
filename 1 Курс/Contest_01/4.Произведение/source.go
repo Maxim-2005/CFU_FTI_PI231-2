e0a503a697f5171b9e066c595bdf2e99961871e0
Maxim <55892094+Maxim-2005@users.noreply.github.com>
2023-10-10 22:30:24 +0300

    Update source.go

--- 1 Курс/Contest_01/4.Произведение/source.go
+++ 1 Курс/Contest_01/4.Произведение/source.go
@@ -1,12 +1,16 @@
 package main
+// Импорт библиотеки
 import "fmt"
 
 func main() {
-    
+
+    // Объявление переменных
     var a, b int
-    
+
+    // Получение значения переменных с консоли
     fmt.Scan(&a)
     fmt.Scan(&b)
-    
+
+    // Вывод умножения
     fmt.Print(a * b)
 }
