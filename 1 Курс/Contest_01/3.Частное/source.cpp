1e0cffdf376ebaddda97eabc131f6e36735058de
Maxim <55892094+Maxim-2005@users.noreply.github.com>
2023-10-10 22:29:20 +0300

    Update source.cpp

--- 1 Курс/Contest_01/3.Частное/source.cpp
+++ 1 Курс/Contest_01/3.Частное/source.cpp
@@ -1,13 +1,17 @@
+// Подключение библиотек
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main(){
+    // Объявление переменных
     double a = 0;
     double b = 0;
-    
+
+    // Запрос знчений переменных с консоли
     cin >> a, cin >> b;
-    
+
+    // Деление и вывод
     cout << a / b;
 }
