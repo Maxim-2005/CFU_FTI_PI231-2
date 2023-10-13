39c1a46b57de180f4af519372285123e70c05af7
Maxim <55892094+Maxim-2005@users.noreply.github.com>
2023-10-13 11:05:11 +0300

    Update source.go

--- 1 Курс/Contest_01/2.Кислород/source.go
+++ 1 Курс/Contest_01/2.Кислород/source.go
@@ -1,20 +1,13 @@
 package main
-// Подключение библиотек
 import "math"
 import "fmt"
 
-// Объявление переменных
-var c, d, t float64
+var c, d, t float64 // литры, дубы, тополя
 
 func main() {
-
-    // Всего литров воздуха за год
     c = 0.5 * 365
-    // Сколько нужно дубов
     d = c / 20
-    // Сколько нужно тополей
     t = c / 32
 
-    // Вывод ответа
     fmt.Print(c, math.Ceil(t), math.Ceil(d))
 }
