27a001fe9bd50fa73e919b7b685ebb17337b6e95
Maxim <55892094+Maxim-2005@users.noreply.github.com>
2023-10-18 23:53:05 +0300

    Update source.go

--- 1 Курс/Contest_03/9. Подходящая пара чисел/source.go
+++ 1 Курс/Contest_03/9. Подходящая пара чисел/source.go
@@ -3,17 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var n, x, k, t, a, b int
-	var arr []int
-	var p bool
+	var n, x, k, t, a, b int // кол-во чисел, требуемая сумма, число, временная переменная для проверки, переменные для вывода ответа
+	var arr []int // массив чисел
+	var p bool // флаг для проверки необходимости добавляения числа в массив
 
 	a, b = 0, 0
 	fmt.Scan(&n, &x)
+	// цикл принимающий все значения
 	for i := 0; i < n; i++ {
 		fmt.Scan(&k)
+		// проверка числа относительно х
 		if k < x {
 			p = true
 			t = x - k
+			// Обход массива и нахождение пар
 			for _, j := range arr {
 				if j == t {
 					if k > j {
