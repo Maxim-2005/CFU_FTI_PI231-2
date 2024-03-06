b2fb503be28b68483acbc811b28373262e9c50d2
Maxim-2005 <maxim2005ua@gmail.com>
2024-03-06 23:33:42 +0300

    A

---
+++ 1 Курс/ALG/Contest_04/2. Количество выполненных задач/2. Количество выполненных задач.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include "dependencies\single_include\nlohmann\json.hpp"
+
+int main() {
+    int n; // идентификатор пользователя
+    int s = 0; // сумма решенных задач
+
+    // чтение файла в объект json
+    std::ifstream file("data.json");
+    nlohmann::json data;
+    file >> data;
+
+    std::cin >> n;
+
+    // обход файла и поиск необходимых значений
+    for (const auto& project : data) {
+        for (const auto& task : project["tasks"]) {
+            if (task["user_id"] == n && task["completed"] == true) {
+                s++;
+            }
+        }
+    }
+
+    std::cout << s << std::endl;
+
+    return 0;
+}
