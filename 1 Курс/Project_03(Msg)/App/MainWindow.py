a82e475f840b44e43bc1400ca10dec6a0b337931
Maxim <maxim2005ua@gmail.com>
2024-03-30 14:41:49 +0300

    Начал добавлять кнопки

--- 1 Курс/Project_03(Msg)/App/MainWindow.py
+++ 1 Курс/Project_03(Msg)/App/MainWindow.py
@@ -8,8 +8,24 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.send.clicked.connect(self.hello)
-
+        
+        self.ui.send.clicked.connect(self.hello) # Надо добавить кнопку
+        
+        #self.ui.notifications_active.connect(self.notifications_active) # Уведомления
+        #self.ui.settings.connect(self.settings) # Настройки
+        #self.ui.theme.connect(self.theme) # Темы
+        #self.ui.user.connect(self.user) # Пользователь
+        #self.ui.find.connect(self.find) # Поиск
+        #self.ui.profile.connect(self.profile) # Профиль
+        #self.ui.menu.connect(self.menu) # Меню
+        
+        
+        
+    # Функции клиентской части месседжера
+    # Отправка логина пароля
+    # Принять список комнат
+    # Обновить чат
+    # Отправить сообщение
     def hello(self):
         text = self.ui.text.text()
         messages.append(text)
@@ -17,3 +33,19 @@ class MainWindow(QMainWindow):
         for i in messages:
             temp = temp + i + "\n"
         self.ui.chat.setText(temp)
+    # Сохранить чат
+    # Создать комнату
+    # Добавить/удалить участников
+    # ? - отправить сообщение с вложением
+    # ? - Скачать файл
+
+
+
+
+    #self.ui.notifications_active.connect(self.notifications_active) # Уведомления
+    #self.ui.settings.connect(self.settings) # Настройки
+    #self.ui.theme.connect(self.theme) # Темы
+    #self.ui.user.connect(self.user) # Пользователь
+    #self.ui.find.connect(self.find) # Поиск
+    #self.ui.profile.connect(self.profile) # Профиль
+    #self.ui.menu.connect(self.menu) # Меню
