c38507105ab1284611408d0039b85169710d5ff1
Maxim <maxim2005ua@gmail.com>
2024-03-31 20:32:29 +0300

    2

--- 1 Курс/Project_03(Msg)/App/MainWindow.py
+++ 1 Курс/Project_03(Msg)/App/MainWindow.py
@@ -1,51 +1,60 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
-from ui_mainwindow import Ui_MainWindow
-
-messages = []
+from ui_mainwindow import Ui_Form
+from chat import chat
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_Form()
         self.ui.setupUi(self)
+        self.upMessages()
         
-        self.ui.send.clicked.connect(self.hello) # Надо добавить кнопку
-        
-        #self.ui.notifications_active.connect(self.notifications_active) # Уведомления
-        #self.ui.settings.connect(self.settings) # Настройки
-        #self.ui.theme.connect(self.theme) # Темы
-        #self.ui.user.connect(self.user) # Пользователь
-        #self.ui.find.connect(self.find) # Поиск
-        #self.ui.profile.connect(self.profile) # Профиль
-        #self.ui.menu.connect(self.menu) # Меню
-        
-        
-        
-    # Функции клиентской части месседжера
-    # Отправка логина пароля
-    # Принять список комнат
-    # Обновить чат
+        self.ui.send.clicked.connect(self.send) # Надо добавить кнопку
+        self.ui.friends.clicked.connect(self.friends)
+        self.ui.notifications_active.clicked.connect(self.notifications_active)
+        self.ui.settings.clicked.connect(self.settings) # Настройки +
+        self.ui.theme.clicked.connect(self.theme) # Темы +
+        self.ui.user.clicked.connect(self.user) # Пользователь +
+        self.ui.profile.clicked.connect(self.profile) # Профиль +
+        self.ui.menu.clicked.connect(self.menu) # Меню +
+
     # Отправить сообщение
-    def hello(self):
-        text = self.ui.text.text()
-        messages.append(text)
-        temp = ""
-        for i in messages:
-            temp = temp + i + "\n"
-        self.ui.chat.setText(temp)
-    # Сохранить чат
-    # Создать комнату
-    # Добавить/удалить участников
-    # ? - отправить сообщение с вложением
-    # ? - Скачать файл
+    def send(self):
+        text = self.ui.message.text()
+        chat.append(text)
+        self.ui.message.setText("")
+        self.upMessages()
 
+    def upMessages(self):
+        msg = "start\n"
+        for i in chat:
+            msg = msg + i + "\n"
+        self.ui.messages.setText(msg)
 
+    # Открыть список друзей
+    def friends(self):
+        print("Friends")
+        
+    # Посмореть уведомления
+    def notifications_active(self):
+        print("notifications_active")
+        
+    # Настройки
+    def settings(self):
+        print("settings")
+    
+    # Темы
+    def theme(self):
+        print("theme")
 
-
-    #self.ui.notifications_active.connect(self.notifications_active) # Уведомления
-    #self.ui.settings.connect(self.settings) # Настройки
-    #self.ui.theme.connect(self.theme) # Темы
-    #self.ui.user.connect(self.user) # Пользователь
-    #self.ui.find.connect(self.find) # Поиск
-    #self.ui.profile.connect(self.profile) # Профиль
-    #self.ui.menu.connect(self.menu) # Меню
+    # Пользователь
+    def user(self):
+        print("user")
+        
+    # Профиль
+    def profile(self):
+        print("profile")
+        
+    # Меню
+    def menu(self):
+        print("menu")
