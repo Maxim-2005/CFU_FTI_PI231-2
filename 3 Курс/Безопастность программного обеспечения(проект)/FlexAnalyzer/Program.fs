d382eaf075db6f346a0b987a4a8c087b4380b430
Maxim <55892094+Maxim-2005@users.noreply.github.com>
2025-10-05 15:19:42 +0300

    Проект на Фшарепе начало

---
+++ 3 Курс/Безопастность программного обеспечения(проект)/FlexAnalyzer/Program.fs
@@ -0,0 +1,37 @@
+﻿(*
+    Project:
+        FlexAnalyzer - Text Analysis Application
+    
+    Description:
+        Приложение для идентификации авторства текста
+    
+    Technical Stack:
+        Language: F#
+        Framework: .NET 8.0
+        GUI: WPF
+        IDE: Visual Studio 2022
+    
+    Author: Максим Покидько
+    University: КФУ ФТИ ПИ
+    Version: 1.0.0
+    Created: 2025-10-05
+*)
+
+open System
+open System.Windows
+
+
+[<STAThread>]
+[<EntryPoint>]
+let main(_) =
+    let app = Application()
+
+    let window = 
+        Uri("MainWindow.xaml", UriKind.Relative)
+        |> Application.LoadComponent
+        :?> Window
+        |> MainWindow.initializeWindow
+
+    app.MainWindow <- window
+    window.Show()
+    app.Run()
